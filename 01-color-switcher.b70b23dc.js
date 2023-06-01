89384fe2e7e69903450da8b8e7f7b55b349ef752
ihor-kutsenko <96702664+ihor-kutsenko@users.noreply.github.com>
2023-06-01 17:11:06 +0000

    Deploying to gh-pages from @ ihor-kutsenko/goit-js-hw-09@0f4f847869bca47acff1e6367c48e541e69513f3 🚀

--- 01-color-switcher.b70b23dc.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");let a=null;function l(){e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(()=>{a=setInterval(l,1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,d.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.b70b23dc.js.map
