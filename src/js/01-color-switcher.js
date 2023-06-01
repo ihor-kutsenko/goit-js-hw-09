a956256d6a26e593c33a0894b189845a9cb9e5cc
Rammbest <igor-kutsenko@ukr.net>
2023-06-01 12:03:04 +0300

    add task1 - color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const bodyEl = document.querySelector("body");
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+start.addEventListener('click', () => {
+  timerId = setInterval(changeBodyColor, 1000);
+  start.disabled = true;
+  stop.disabled = false;
+}
+);
+
+stop.addEventListener('click', () => {
+  clearInterval(timerId);
+  start.disabled = false;
+  stop.disabled = true;
+}
+)
+
+function changeBodyColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+ };
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
